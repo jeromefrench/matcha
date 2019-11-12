9b6d6f29aa93f92a298cf0d4f58590d15de9d355
jchardin <jerome.chardin@outook.com>
2019-11-12 16:26:59 +0100

    research en cours

--- controler/research.js
+++ controler/research.js
@@ -1,10 +1,14 @@
-let bdd = require('../models/bdd_functions.js');
+let bdd = require('../models/research.js');
 
 
 module.exports.ctrl_researchGet = function profileLoginGet(req, res){
+
+	uneVariable = 3;
+	bdd.get_user(uneVariable, (all_user) => {
+		res.locals.users = all_user;
+	});
     res.render('research.ejs', {session: req.session});
 }
-
 
 module.exports.ctrl_researchPost = function profileLoginPost(req, res){
 }
