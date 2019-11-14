b075450072e21dee1ebb6015e50915b4635ca93f
jchardin <jerome.chardin@outook.com>
2019-11-14 19:34:33 +0100

    about you

--- controler/about_you.js
+++ controler/about_you.js
@@ -3,7 +3,21 @@ let bdd = require('../models/bdd_functions.js');
 
 module.exports.ctrl_aboutYouGet = function aboutYouGet(req, res){
 	res.locals.title = "About You";
-	res.render('about-you.ejs', {session: req.session});
+
+	bdd_about.get_info_user(req.session.login, (result) => {
+		console.log(result);
+		if (result != undefined) {
+			console.log("ici");
+		   res.locals.infos = result[0];
+		}
+		console.log("on log");
+		console.log(result);
+		console.log(res.locals);
+		res.render('about-you.ejs', {session: req.session});
+	})
+
+
+
 }
 
 module.exports.ctrl_aboutYouPost = function aboutYouPost(req, res){
@@ -11,13 +25,13 @@ module.exports.ctrl_aboutYouPost = function aboutYouPost(req, res){
 	orientation = req.body.orientation;
 	bio = req.body.bio;
 	interests = req.body.interests;
-	bdd.get_id_user( (id_user) => {
+	bdd.get_id_user(req.session.login,  (id_user) => {
 		id_user = id_user;
 		bdd_about.is_info_user_exist(req.session.login, (userExist) => {
 			if (userExist){
-bdd_about.update_info_user(id_user, gender, orientation, bio, interests);
+				bdd_about.update_info_user(id_user, gender, orientation, bio, interests);
 			} else {
-bdd_about.insert_info(id_user, gender, orientation, bio, interests)
+				bdd_about.insert_info_user(id_user, gender, orientation, bio, interests)
 			}
 		})
 	});
--- models/about_you.js
+++ models/about_you.js
@@ -1,6 +1,7 @@
 let bdd = require('../models/bdd_functions.js');
+var conn = require('./connection_bdd.js');
 
-exports.get_photo = function (login){
+exports.get_photo = function (login, callback){
 	bdd.get_id_user(login, (id_login) => {
 		var sql = "SELECT * FROM `photos` WHERE `id_user` = ?";
 		var todo = [id_login];
@@ -11,26 +12,77 @@ exports.get_photo = function (login){
 	});
 }
 
+exports.get_info_user = function (login, callback){
+	bdd.get_id_user(login, (id_login) => {
+		var sql = "SELECT * FROM `user_info` WHERE `id_user` = ?";
+		var todo = [id_login];
+		conn.connection.query(sql, todo, (error, result) => {
+			if (error) throw error;
+			callback(result);
+		});
+	});
+}
+
 exports.insert_info_user = function (id_user, gender, orientation, bio, interests){
-	var sql = "	INSERT INTO `info_user` (`id_user`, `gender`, `orientation`, `bio`, `interests`) VALUES (?, ?, ?, ?, ?, ?);";
+	var sql = "	INSERT INTO `user_info` (`id_user`, `gender`, `orientation`, `bio`, `interests`) VALUES (?, ?, ?, ?, ?);";
 	var todo = [id_user, gender, orientation, bio, interests];
 	conn.connection.query(sql, todo, (error, result) => {
 		if (error) throw error;
-		callback(result);
 	});
 }
 
 exports.update_info_user = function (id_user, gender, orientation, bio, interests){
-	var sql = "UPDATE `info_user` SET `gender` = ?, `orientation` = ?, `bio` = ?, `interests` = ?  WHERE `id_user` = ?";
-	var todo = [gender, orientation, bio, interests, id_user];
-	conn.connection.query(sql, todo, (error, result) => {
-		if (error) throw error;
-		callback(result);
-	});
+	var sql = "UPDATE `user_info` SET `gender` = ?, `orientation` = ?, `bio` = ?, `interests` = ?  WHERE `id_user` = ?";
+	console.log("interest =>");
+	console.log(interests);
+	inter = "";
+	itemsProcessed = 0;
+	if (interests !=undefined){
+		interests.forEach(function(interest) {
+			if (inter == ""){
+				console.log("1");
+				console.log(inter);
+				console.log(interest);
+				inter += interest;
+			}else{
+				console.log("2");
+				console.log(inter);
+				console.log(interest);
+				inter += "," + interest;
+			}
+			itemsProcessed++;
+			if(itemsProcessed === interests.length) {
+				console.log("interest =>=>");
+				console.log(inter);
+				var todo = [gender, orientation, bio, inter, id_user];
+				conn.connection.query(sql, todo, (error, result) => {
+					if (error) throw error;
+				})
+			}
+		});
+	}
+	else {
+		inter = null;
+		var todo = [gender, orientation, bio, inter, id_user];
+		conn.connection.query(sql, todo, (error, result) => {
+			if (error) throw error;
+		});
+	}
 }
 
-exports.is_info_user_exist = function (login){
-	bdd.get_id_user( (id_user) => {
+
+
+
+
+
+
+
+
+
+
+
+exports.is_info_user_exist = function (login, callback){
+	bdd.get_id_user(login,  (id_user) => {
 		var sql = "SELECT * FROM `user_info` WHERE `id_user` = ?";
 		var todo = [id_user];
 		conn.connection.query(sql, todo, (error, result) => {
@@ -44,3 +96,21 @@ exports.is_info_user_exist = function (login){
 		});
 	})
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
