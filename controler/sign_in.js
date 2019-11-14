5c32b4f21e3ddda0fab0102f327eb5600925d762
Laetitia LEJEUNE <llejeune@e1r2p20.42.fr>
2019-11-14 19:16:20 +0100

    champs prerempli sign in

--- controler/sign_in.js
+++ controler/sign_in.js
@@ -8,14 +8,17 @@ module.exports.ctrl_signInGet = function sign_in(req, res){
 module.exports.ctrl_signInPost = function signInPost(req, res){
 	console.log("la console" + req.body.login);
 	var login = req.body.login;
+	req.session.login = login;
 	bdd.check_log(login, (result, result1) => {
 		req.session.logexist = undefined;
 		if (result == 0 && result1 == 0){
 			req.session.logexist = 2;
+			req.session.login = undefined;
 			res.redirect('/sign-in');
 		}
 		else if (result1 > 0){
 			req.session.logexist = 3;
+			req.session.login = undefined;
 			res.redirect('/sign-in');
 		}
 		else if (result > 0){
