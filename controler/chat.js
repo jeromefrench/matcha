5714b1a1f0217c8ef25c319751fa50e0afa1764f
jchardin <jerome.chardin@outook.com>
2019-11-14 13:38:09 +0100

    chat fonctionel

--- controler/chat.js
+++ controler/chat.js
@@ -4,21 +4,29 @@ var bdd_message = require('../models/message.js');
 module.exports.ctrl_chatGet = function chatGet(req, res){
 	res.locals.my_login = req.session.login;
 	res.locals.the_login_i_chat = req.params.login;
-	bdd_message.get_message(req.session.login, req.params.login, (result) => {
-		console.log(result);
-		res.locals.messages = result;
-    	res.render('chat', { session: req.session});
-	})
+	author = req.session.login;
+	le_recever = req.params.login;
+	bdd.get_id_user(author, (result) => {
+		id_author = result;
+		bdd.get_id_user(le_recever, (result) => {
+			id_recever = result;
+			bdd_message.get_message(id_author, id_recever, (result) => {
+				console.log("JE VEUS LES RESULTAT");
+				console.log(result);
+				res.locals.messages = result;
+    			res.render('chat', { session: req.session});
+			})
+		});
+	});
 }
-
-
 
 module.exports.ctrl_chatPost = function chatPost(req, res){
 	author = req.session.login;
+	le_recever = req.params.login;
+	login = le_recever;
 	bdd.get_id_user(author, (result) => {
 		//l'autheur id_author
 		id_author = result;
-		le_recever = req.params.login;
 		bdd.get_id_user(le_recever, (result) => {
 			//le receveur id_recever
 			id_recever = result;
--- models/Message.js
+++ models/Message.js
@@ -1,9 +1,8 @@
 var conn = require('./connection_bdd.js');
 
-
 exports.save_message = function (id_author, id_recever, message_content, data_stamp){
-	var sql = "INSERT INTO `messages` (id_author, id_recever, message_content, data_stamp) VALUES (?, ?, ?, ?, ?)";
-	var todo = [my_id, id_i_like];
+	var sql = "INSERT INTO `messages` (id_author, id_recever, message_content, data_stamp) VALUES (?, ?, ?, ?)";
+	var todo = [id_author, id_recever, message_content, data_stamp];
 	conn.connection.query(sql, todo, (error, result) => {
 		if (error) throw error;
 		console.log("infos added");
@@ -11,10 +10,14 @@ exports.save_message = function (id_author, id_recever, message_content, data_st
 }
 
 exports.get_message = function (id_author, id_recever, callback){
-	var sql = "SELECT * FROM `messages` WHERE `id_author` = ? AND `id_recever` = ?";
-	var todo = [id_author, id_recever];
+	console.log("l'id author " + id_author);
+	console.log("l'id recever " + id_recever);
+	var sql = "SELECT * FROM `messages` WHERE `id_author` = ? AND `id_recever` = ? OR `id_recever` = ? AND `id_author` = ?";
+	var todo = [id_author, id_recever, id_author, id_recever];
 	conn.connection.query(sql, todo, (error, result) => {
 		if (error) throw error;
+		console.log("les resultat !!!!" + result);
+		console.log(result);
 		callback(result);
 	});
 }
