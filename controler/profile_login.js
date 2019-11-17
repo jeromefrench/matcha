dac0ebc01032ec8462832b333c6d828730a06f32
jchardin <jerome.chardin@outook.com>
2019-11-17 21:00:45 +0100

    profile ongoing

--- controler/profile_login.js
+++ controler/profile_login.js
@@ -1,12 +1,27 @@
 let bdd = require('../models/bdd_functions.js');
+let bdd_re = require('../models/research.js');
+let bdd_like = require('../models/like.js');
 
 
 module.exports.ctrl_profileLoginGet = function profileLoginGet(req, res){
 	res.locals.login_profil = req.params.login;
 	res.locals.session = req.session;
-    res.render('profile.ejs');
-}
 
-
-module.exports.ctrl_profileLoginPost = function profileLoginPost(req, res){
+	bdd_re.get_user_profile(req.params.login, (result) => {
+		console.log(result[0]);
+		user = result[0];
+		bdd_like.doILike(req.session.login, user.login, function (result){
+			user.do_i_like = result;
+			bdd_like.doesItLikeMe(req.session.login, user.login, function (result){
+				user.does_it_like_me = result;
+				if (user.do_i_like && user.does_it_like_me){
+					user.match = true;
+				}
+				else {
+					user.match = false;
+				}
+    			res.render('profile.ejs', {session: req.session, user: user});
+			});
+		})
+	})
 }
--- models/research.js
+++ models/research.js
@@ -29,3 +29,15 @@ exports.get_user = function (login, callback){
 		// });
 	})
 }
+
+
+exports.get_user_profile = function (login, callback){
+	bdd.get_id_user(login, (id_user) => {
+		var sql = "	SELECT * FROM `docker`.`user` INNER JOIN `docker`.`user_info` ON `docker`.`user`.`id` = `docker`.`user_info`.`id_user` INNER JOIN `docker`.`photo` ON `docker`.`user`.`id` = `docker`.`photo`.`id_user` WHERE `docker`.`user`.`id` = ? AND `docker`.`user_info`.`completed` = 1 AND `docker`.`photo`.`profile` = 1";
+		var todo = [id_user];
+		conn.connection.query(sql, todo, (error, result) => {
+			if (error) throw error;
+			callback(result);
+		});
+	})
+}
