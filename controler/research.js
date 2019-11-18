234754c4b300210df2d909237a9b68d2c742fb7d
jchardin <jerome.chardin@outook.com>
2019-11-18 11:58:54 +0100

    fix research when only one user

--- controler/research.js
+++ controler/research.js
@@ -6,25 +6,41 @@ module.exports.ctrl_researchGet = function profileLoginGet(req, res){
 	uneVariable = 3;
 	itemsProcessed = 0;
 	bdd.get_user(req.session.login, (all_user) => {
+		console.log("ici");
 		console.log(all_user);
-		all_user.forEach(function (user){
-			bdd_like.doILike(req.session.login, user.login, function (result){
-				user.do_i_like = result;
-				bdd_like.doesItLikeMe(req.session.login, user.login, function (result){
-					user.does_it_like_me = result;
-					if (user.do_i_like && user.does_it_like_me){
-						user.match = true;
-					}
-					else {
-						user.match = false;
-					}
-					itemsProcessed++;
-					if(itemsProcessed === all_user.length) {
-						res.locals.users = all_user;
-    					res.render('research.ejs', {session: req.session});
-    				}
-				});
-			})
-		});
+
+		if (all_user[0] == undefined){
+    		res.render('research.ejs', {session: req.session});
+		}else{
+			all_user.forEach(function (user){
+				console.log("ici");
+				bdd_like.doILike(req.session.login, user.login, function (result){
+					console.log("ici");
+					user.do_i_like = result;
+					bdd_like.doesItLikeMe(req.session.login, user.login, function (result){
+						console.log("ici");
+						user.does_it_like_me = result;
+						if (user.do_i_like && user.does_it_like_me){
+							user.match = true;
+						}
+						else {
+							user.match = false;
+						}
+						itemsProcessed++;
+						console.log(all_user.length);
+						console.log(itemsProcessed);
+						if(itemsProcessed === all_user.length) {
+							res.locals.users = all_user;
+						console.log("iciaaa");
+							console.log(res.locals.users);
+    						res.render('research.ejs', {session: req.session});
+    					}
+					});
+				})
+			});
+
+		}
+
+
 	});
 };
--- serveur.js
+++ serveur.js
@@ -1,14 +1,5 @@
 
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Slider, { Range } from 'rc-slider';
-// We can just import Slider or Range to reduce bundle size
-// import Slider from 'rc-slider/lib/Slider';
-// import Range from 'rc-slider/lib/Range';
-import 'rc-slider/assets/index.css';
-
-
 
 
 let express = require('express');
