fc1d046b2595953c63b1314a223e5c63c3cfff4c
jchardin <jerome.chardin@outook.com>
2019-12-04 13:12:19 +0100

    change the year range for 1900 to 2019

--- controler/about_you.js
+++ controler/about_you.js
@@ -33,23 +33,20 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/').post((req, res) => {
-	//*************************************************************************
+	req.session.try_locali = 0;
 	localisation = {};
 	localisation['country'] = req.body.country;
 	localisation['city' ] = req.body.city;
 	localisation['zipcode'] = req.body.zipcode;
-	req.session.try_locali = 0;
-	req.session.localisation = localisation;
-	//*************************************************************************
 	gender = req.body.gender;
 	orientation = req.body.orientation;
 	bio = req.body.bio;
-	birthday = req.body.birthday;
-	var birthday = birthday.split("/");
-	birthday = birthday[2]+ "-"+ birthday[0]+"-"+birthday[1];
 	interests = req.body.interests;
+	birthday = req.body.birthday;
+	birthday = birthday.split("/");
+	birthday = birthday[2]+ "-"+ birthday[0]+"-"+birthday[1];
+
 	bdd.get_id_user(req.session.login,  (id_user) => {
-		id_user = id_user;
 		bdd_about.is_info_user_exist(req.session.login, (userExist) => {
 			if (userExist){
 				bdd_about.update_info_user(id_user, gender, orientation, bio, interests, birthday);
@@ -59,88 +56,17 @@ router.route('/').post((req, res) => {
 				addPicture(id_user, req, rootPath);
 			}
 			//*****************************************************************
-			localisation = {};
-			localisation['country'] = req.body.country;
-			localisation['city' ] = req.body.city;
-			localisation['zipcode'] = req.body.zipcode;
-			req.session.try_locali = 0;
+			//Localisation
+			//*****************************************************************
 			if (req.body.latitude && req.body.latitude != "latitude" && req.body.latitude != "cherche_ip"){
-				local = req.body.latitude + ", " + req.body.longitude;
-				opencage.geocode({q: local, language: 'fr'}).then(data => {
-					if (data.status.code == 200) {
-						if (data.results.length > 0) {
-							var place = data.results[0];
-							localisation['country'] = place.components.country;
-							localisation['city' ] = place.components.city;
-							localisation['zipcode'] = place.components.postcode;
-							localisation['latitude'] = req.body.latitude;
-							localisation['longitude'] = req.body.longitude;
-							bdd_about.insert_info_user_localalisation(req.session.login, localisation['country'], localisation['city'], localisation['zipcode'], localisation['longitude'], localisation['latitude'], () => {
-								res.redirect('/about-you');
-							})
-						}
-					} else if (data.status.code == 402) {
-						res.redirect('/about_you');
-					} else {
-						res.redirect('/about-you');
-					}
-				}).catch(error => {
-					console.log('error', error.message);
-					res.redirect('/about-you');
-				});
-
+				searchLatitudeLongitude(req, res);
 			} else if (req.body.submit_button == "Localise Me") {
 				req.session.try_locali = 1;
 				res.redirect('/about-you');
-			} else if (localisation['country'] != "" &&
-				localisation['city'] != "" &&
-				localisation['zipcode'] != ""){
-				loc = localisation['country'] + "  " + localisation['city'] + "  " + localisation['zipcode'];
-				opencage.geocode({q: '' + loc}).then(data => {
-					if (data.status.code == 200) {
-						if (data.results.length > 0) {
-							var place = data.results[0];
-							localisation['country'] = place.components.country;
-							localisation['city' ] = place.components.city;
-							localisation['zipcode'] = place.components.postcode;
-							localisation['latitude'] = place.geometry.lat;
-							localisation['longitude'] = place.geometry.lng;
-							bdd_about.insert_info_user_localalisation(req.session.login, localisation['country'], localisation['city'], localisation['zipcode'], localisation['longitude'], localisation['latitude'], () => {
-								res.redirect('/about-you');
-							})
-						}
-					} else if (data.status.code == 402) {
-					} else {
-					}
-				}).catch(error => {
-					console.log('error', error.message);
-				});
+			} else if (localisation['country'] != "" && localisation['city'] != "" && localisation['zipcode'] != ""){
+				searchAdresse(req, res);
 			} else if(req.body.latitude && req.body.latitude == "cherche_ip") {
-				var ip = req.clientIp;
-				ip = "62.210.34.185";
-				var endpoint = 'http://ip-api.com/json/'+ip+'?fields=status,message,country,city,zip,lat,lon,query';
-				var xhr = new XMLHttpRequest();
-				xhr.open('GET', endpoint, true);
-				xhr.send();
-				xhr.onreadystatechange = function() {
-					if (this.readyState == 4 && this.status == 200) {
-						var response = JSON.parse(this.responseText);
-						if(response.status !== 'success') {
-							return
-						}
-						else {
-							localisation = {};
-							localisation['country'] = response.country;
-							localisation['city' ] = response.city;
-							localisation['zipcode'] = response.zip;
-							localisation['latitude'] = response.lat;
-							localisation['longitude'] = response.lon;
-							bdd_about.insert_info_user_localalisation(req.session.login, localisation['country'], localisation['city'], localisation['zipcode'], localisation['longitude'], localisation['latitude'], () => {
-								res.redirect('/about-you');
-							})
-						}
-					}
-				};
+				searchIp(req, res);
 			}else{
 				res.redirect('/about-you');
 			}
@@ -148,6 +74,87 @@ router.route('/').post((req, res) => {
 	});
 });
 
+
+
+
+
+function searchIp(req, res){
+	var ip = req.clientIp;
+	ip = "62.210.34.185";
+	var endpoint = 'http://ip-api.com/json/'+ip+'?fields=status,message,country,city,zip,lat,lon,query';
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', endpoint, true);
+	xhr.send();
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var response = JSON.parse(this.responseText);
+			if(response.status !== 'success') {
+				return
+			}
+			else {
+				localisation = {};
+				localisation['country'] = response.country;
+				localisation['city' ] = response.city;
+				localisation['zipcode'] = response.zip;
+				localisation['latitude'] = response.lat;
+				localisation['longitude'] = response.lon;
+				bdd_about.insert_info_user_localalisation(req.session.login, localisation['country'], localisation['city'], localisation['zipcode'], localisation['longitude'], localisation['latitude'], () => {
+					res.redirect('/about-you');
+				})
+			}
+		}
+	};
+
+}
+
+function searchAdresse(req, res){
+	loc = localisation['country'] + "  " + localisation['city'] + "  " + localisation['zipcode'];
+	opencage.geocode({q: '' + loc}).then(data => {
+		if (data.status.code == 200) {
+			if (data.results.length > 0) {
+				var place = data.results[0];
+				localisation['country'] = place.components.country;
+				localisation['city' ] = place.components.city;
+				localisation['zipcode'] = place.components.postcode;
+				localisation['latitude'] = place.geometry.lat;
+				localisation['longitude'] = place.geometry.lng;
+				bdd_about.insert_info_user_localalisation(req.session.login, localisation['country'], localisation['city'], localisation['zipcode'], localisation['longitude'], localisation['latitude'], () => {
+					res.redirect('/about-you');
+				})
+			}
+		} else if (data.status.code == 402) {
+		} else {
+		}
+	}).catch(error => {
+		console.log('error', error.message);
+	});
+}
+
+function searchLatitudeLongitude(req, res){
+	local = req.body.latitude + ", " + req.body.longitude;
+	opencage.geocode({q: local, language: 'fr'}).then(data => {
+		if (data.status.code == 200) {
+			if (data.results.length > 0) {
+				var place = data.results[0];
+				localisation['country'] = place.components.country;
+				localisation['city' ] = place.components.city;
+				localisation['zipcode'] = place.components.postcode;
+				localisation['latitude'] = req.body.latitude;
+				localisation['longitude'] = req.body.longitude;
+				bdd_about.insert_info_user_localalisation(req.session.login, localisation['country'], localisation['city'], localisation['zipcode'], localisation['longitude'], localisation['latitude'], () => {
+					res.redirect('/about-you');
+				})
+			}
+		} else if (data.status.code == 402) {
+			res.redirect('/about_you');
+		} else {
+			res.redirect('/about-you');
+		}
+	}).catch(error => {
+		console.log('error', error.message);
+		res.redirect('/about-you');
+	});
+}
 
 function addPicture(id_user, req, rootPath){
 	if (req.files != undefined && req.files.photo && req.files.photo.size != 0){
