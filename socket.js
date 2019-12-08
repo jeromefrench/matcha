b9aa20ab0ea065c4f3f517e958a35fe448058392
Laetitia LEJEUNE <llejeune@e1r4p9.42.fr>
2019-12-08 15:45:57 +0100

    block ne notif plus

--- socket.js
+++ socket.js
@@ -1,5 +1,30 @@
 const jwt = require('jsonwebtoken');
 var conn = require('./models/connection_bdd.js');
+var bdd = require('./models/bdd_functions.js');
+
+function isBlock(userLog, isblockLog, callback){
+	bdd.get_id_user(userLog, (userId) => {
+		bdd.get_id_user(isblockLog, (isblockId) => {
+			var sql = "SELECT COUNT(*) AS 'count' FROM `block` WHERE `id_user` = ? AND `id_block` = ?";
+			var todo = [userId, isblockId];
+			conn.connection.query(sql, todo, (err, result) => {
+				if (err){console.log(err);}
+				else{
+					callback(result[0].count);
+				}
+			});
+		});
+	});
+	// var sql = "SELECT COUNT(*) AS 'count' FROM `block` WHERE `id_user` = ? AND `id_block` = ?";
+	// var todo = [userLog, isblockLog];
+	// conn.connection.query(sql, todo, (err, result) => {
+	// 	if (err){console.log(err);}
+	// 	else{
+	// 		console.log("block = " + result[0].count);
+	// 		callback(result[0].count);
+	// 	}
+	// });
+}
 
 exports = module.exports = function(io){
 
@@ -39,34 +64,49 @@ exports = module.exports = function(io){
 						}
 						socket.join(currentUser.login, () => {
 							socket.on('vue_profile', (data) => {
-								console.log("hello you ici bas");
-								console.log(data);
-								io.to(data.room).emit('notifvue', {user: currentUser.login});
+								isBlock(data.room, currentUser.login, (block) => {
+									if (block == 0){
+										console.log("hello you ici bas");
+										console.log(data);
+										io.to(data.room).emit('notifvue', {user: currentUser.login});
+									}	
+								});
+								// console.log("hello you ici bas");
+								// console.log(data);
+								// io.to(data.room).emit('notifvue', {user: currentUser.login});
 							});
 							socket.on('message', function(data){
-								console.log("on a un new message");
-    							console.log(data.room);
-    							console.log(data.message);
-    							//if (le current user a matcher avec data room){
-    							io.to(data.room).emit('message', {message: data.message, leUser: currentUser.login});
-    							//}
+								isBlock(data.room, currentUser.login, (block) => {
+									if (block == 0){
+										console.log("on a un new message");
+    									console.log(data.room);
+    									console.log(data.message);
+    									//if (le current user a matcher avec data room){
+    									io.to(data.room).emit('message', {message: data.message, leUser: currentUser.login});
+										//}
+									}
+								});
 							});
 							socket.on('like', (data) => {
-								if (data.like == 1){
-									var sql = "SELECT COUNT(*) AS 'count' FROM `like_table` INNER JOIN `user` ON `docker`.`user`.`id` = `docker`.`like_table`.`id_user` INNER JOIN `photo` ON `docker`.`photo`.`id_user` = `docker`.`like_table`.`id_user` WHERE `id_i_like` = ? AND `like_table`.`id_user` IN (SELECT `id_i_like` FROM `like_table` WHERE `id_user` = ?)";
-									var todo = [currentUser.id, currentUser.id];
-									conn.connection.query(sql, todo, (err, result) => {
-										if (err) {console.log(err);}
-										console.log('bla***********************');
-										console.log(result[0].count);
-										if (result[0].count != 0){
-											io.to(data.room).emit('notifmatch', {user: currentUser.login});
+								isBlock(data.room, currentUser.login, (block) => {
+									if (block == 0){
+										if (data.like == 1){
+											var sql = "SELECT COUNT(*) AS 'count' FROM `like_table` INNER JOIN `user` ON `docker`.`user`.`id` = `docker`.`like_table`.`id_user` INNER JOIN `photo` ON `docker`.`photo`.`id_user` = `docker`.`like_table`.`id_user` WHERE `id_i_like` = ? AND `like_table`.`id_user` IN (SELECT `id_i_like` FROM `like_table` WHERE `id_user` = ?)";
+											var todo = [currentUser.id, currentUser.id];
+											conn.connection.query(sql, todo, (err, result) => {
+												if (err) {console.log(err);}
+												console.log('bla***********************');
+												console.log(result[0].count);
+												if (result[0].count != 0){
+													io.to(data.room).emit('notifmatch', {user: currentUser.login});
+												}
+												else if (result[0].count == 0){
+													io.to(data.room).emit('notiflike', {user: currentUser.login});
+												}
+											});
 										}
-										else if (result[0].count == 0){
-											io.to(data.room).emit('notiflike', {user: currentUser.login});
-										}
-									});
-								}
+									}
+								});
 							});
 						})
 						console.log("tableau user");
