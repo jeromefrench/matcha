5e36aa6dbe8395c5f1820dbef04fea909a2f2308
jchardin <jerome.chardin@outook.com>
2019-12-25 18:21:33 +0100

    profile page working again

--- controler/like-this-user.js
+++ controler/like-this-user.js
@@ -3,28 +3,27 @@ const router = require('express').Router();
 var bdd_notif = require('../models/notifications.js');
 
 //router.route('/:login').get((req, res) => {
-router.route('/:login').post((req, res) => {
+router.route('/:login').post(async (req, res) => {
 	my_login = req.session.login;
 	the_login_i_like = req.params.login;
-	bdd.doILike(my_login, the_login_i_like, (result) => {
-		if (result){
-			bdd.unLike(my_login, the_login_i_like);
-			// bdd_notif.save_notif(my_login, the_login_i_like, my_login + " doesn't like you anymore", (result) => {
-			// });
-			//res.redirect('back');
-			res.send("ok");
-		}
-		else {
-			bdd.addLike(my_login, the_login_i_like);
-			// req.io.
-			bdd.updateMatch(my_login, the_login_i_like);
-			bdd.updateMatch(the_login_i_like, my_login);
-			bdd_notif.save_notif(my_login, the_login_i_like, my_login + " likes you!", (result) => {
-			});
-			//res.redirect('back');
-			res.send("ok");
-		}
-	});
+	result = await bdd.doILike(my_login, the_login_i_like);
+	if (result){
+		bdd.unLike(my_login, the_login_i_like);
+		// bdd_notif.save_notif(my_login, the_login_i_like, my_login + " doesn't like you anymore", (result) => {
+		// });
+		//res.redirect('back');
+		res.send("ok");
+	}
+	else {
+		bdd.addLike(my_login, the_login_i_like);
+		// req.io.
+		bdd.updateMatch(my_login, the_login_i_like);
+		bdd.updateMatch(the_login_i_like, my_login);
+		bdd_notif.save_notif(my_login, the_login_i_like, my_login + " likes you!", (result) => {
+		});
+		//res.redirect('back');
+		res.send("ok");
+	}
 });
 
 module.exports = router;
