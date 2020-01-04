55df13e3428a3939638ccf33b0f7882e43d3c46e
jchardin <jerome.chardin@outook.com>
2020-01-04 13:50:41 +0100

    try catch in model

--- controler/photo.js
+++ controler/photo.js
@@ -4,17 +4,22 @@ const router = require('express').Router();
 router.route('/:login/:num/profile').get(async (req, res) => {
 	//puis on met la photo selectionner a 1
 	//on met toute les photos du user a 0
-	var my_login = req.params.login;
+
+
+	//verifier si la photo exist
 	var num = req.params.num;
 	var id_user = await bdd.get_id_user(req.session.login);
 	var done = await bdd.profileToZero(id_user);
-	var path = "/public/photo/"+my_login+"/"+num;
+	var path = "/public/photo/"+req.session.login+"/"+num;
 	done = await bdd.profileToOne(path);
 	res.redirect('/about-you');
 });
 
 router.route('/:login/:num').get(async (req, res) => {
-	var my_login = req.params.login;
+
+
+	//verifier si la photo exist
+	var my_login = req.session.login;
 	var num = req.params.num;
 	var path = "/public/photo/"+my_login+"/"+num;
 	var result = await bdd.isProfile(path);
--- models/about_you.js
+++ models/about_you.js
@@ -1,89 +1,150 @@
-exports.InfoUser = async function (login, property, field){
-	var userExist = await is_info_user_exist(login);
-	if (userExist){
-		update_info_user(login, property, field);
-	} else {
-		insert_info_user(login, property, field);
+module.exports.InfoUser = InfoUser;
+module.exports.count_photo  = count_photo ;
+module.exports.profileToZero  = profileToZero ;
+module.exports.get_id_user  = get_id_user ;
+module.exports.profileToOne  = profileToOne ;
+module.exports.isProfile  = isProfile ;
+module.exports.delPic  = delPic ;
+module.exports.get_photo  = get_photo ;
+module.exports.check_field_about_you   = check_field_about_you  ;
+module.exports.check_field_localisation   = check_field_localisation  ;
+module.exports.check_picture   = check_picture  ;
+module.exports.isCompleted  = isCompleted ;
+module.exports.savethePic  = savethePic ;
+module.exports.get_info_user  = get_info_user ;
+module.exports.get_completed  = get_completed ;
+
+
+async function InfoUser (login, property, field){
+	try {
+		var userExist = await is_info_user_exist(login);
+		if (userExist){
+			update_info_user(login, property, field);
+		} else {
+			insert_info_user(login, property, field);
+		}
+	}
+	catch (err){
+		console.log(err);
+		return (err);
 	}
 }
 
-exports.count_photo = async function count_photo (id_user){
-	var sql = "SELECT count(*) as 'count' FROM `photo` WHERE `id_user` = ?";
-	var todo = [id_user];
-	var result = await db.query(sql, todo);
-	if (result[0] == undefined) {
-		return (0);
+async function count_photo (id_user){
+	try {
+		var sql = "SELECT count(*) as 'count' FROM `photo` WHERE `id_user` = ?";
+		var todo = [id_user];
+		var result = await db.query(sql, todo);
+		if (result[0] == undefined) {
+			return (0);
+		}
+		else {
+			return (result[0].count);
+		}
 	}
-	else {
-		return (result[0].count);
+	catch (err){
+		console.log(err);
+		return (err);
 	}
 }
 
 async function savePic (id_user, path, profile){
-	var sql = "INSERT INTO `photo` (`id_user`, `path_photo`, `profile`) VALUES (?, ?, ?);";
-	var todo = [id_user, path, profile];
-	var result = await db.query(sql, todo);
+	try {
+		var sql = "INSERT INTO `photo` (`id_user`, `path_photo`, `profile`) VALUES (?, ?, ?);";
+		var todo = [id_user, path, profile];
+		var result = await db.query(sql, todo);
+	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
 }
 
-async function get_info_user (login){
-	var id_user = await get_id_user(login);
-	var sql = "SELECT *, DATE_FORMAT(`birthday`, '%Y\\%m\\%d') as birth FROM `user_info` WHERE `id_user` = ?";
-	var todo = [id_user];
-	var result = await db.query(sql, todo);
-	return (result);
-}
 
 async function getPic (id_user){
-	var sql = "SELECT * FROM `photo` WHERE `id_user` = ?";
-	var todo = [id_user];
-	var result = await db.query(sql, todo);
-	if (result[0] == undefined) {
-		return (false);
+	try {
+		var sql = "SELECT * FROM `photo` WHERE `id_user` = ?";
+		var todo = [id_user];
+		var result = await db.query(sql, todo);
+		if (result[0] == undefined) {
+			return (false);
+		}
+		else {
+			return (result);
+		}
 	}
-	else {
+	catch (err){
+		console.log(err);
+		return (err);
+	}
+}
+
+async function get_id_user (login){
+	try {
+		var sql = "SELECT `id` FROM `user` WHERE `login` LIKE ?";
+		var todo = [login];
+		result = await db.query(sql, todo);
+		return (result[0].id);
+	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
+}
+
+async function profileToZero (id_user){
+	try {
+		var sql = "UPDATE `photo` SET `profile`= 0 WHERE `id_user` = ?";
+		var todo = [id_user];
+		result = await db.query(sql, todo);
+		return ("done");
+	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
+}
+
+async function profileToOne (path){
+	try {
+		var sql = "UPDATE `photo` SET `profile`= 1 WHERE `path_photo` LIKE ?";
+		var todo = [path];
+		result = await db.query(sql, todo);
+		return ("done");
+	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
+}
+
+async function isProfile (path){
+	try {
+		var sql = "SELECT `profile` FROM `photo` WHERE `path_photo` LIKE ?";
+		var todo = [path];
+		result = await db.query(sql, todo);
 		return (result);
 	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
 }
 
-exports.get_id_user = async function (login){
-	var sql = "SELECT `id` FROM `user` WHERE `login` LIKE ?";
-	var todo = [login];
-	result = await db.query(sql, todo);
-	return (result[0].id);
+async function delPic (path){
+	try {
+		var sql = "	DELETE FROM `photo` WHERE `path_photo` LIKE ?";
+		var todo = [path];
+		result = await db.query(sql, todo);
+		return "done";
+	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
 }
 
-exports.profileToZero = async function (id_user){
-	var sql = "UPDATE `photo` SET `profile`= 0 WHERE `id_user` = ?";
-	var todo = [id_user];
-	result = await db.query(sql, todo);
-	return ("done");
-}
-
-exports.profileToOne = async function (path){
-	var sql = "UPDATE `photo` SET `profile`= 1 WHERE `path_photo` LIKE ?";
-	var todo = [path];
-	result = await db.query(sql, todo);
-	return ("done");
-}
-
-exports.isProfile = async function (path){
-	var sql = "SELECT `profile` FROM `photo` WHERE `path_photo` LIKE ?";
-	var todo = [path];
-	result = await db.query(sql, todo);
-	return (result);
-}
-
-exports.delPic = async function (path){
-	var sql = "	DELETE FROM `photo` WHERE `path_photo` LIKE ?";
-	var todo = [path];
-	result = await db.query(sql, todo);
-	return "done";
-}
-
-//******************************************************************************
-
-
-exports.check_field_about_you = function (field){
+function check_field_about_you (field){
 	var check_field = {};
 
 	check_field['gender'] = check_gender(field['gender']);
@@ -94,7 +155,7 @@ exports.check_field_about_you = function (field){
 	return check_field;
 }
 
-exports.check_field_localisation = function (field, check_field){
+function check_field_localisation (field, check_field){
 	check_field['country'] = help_noempty(field['country']);
 	check_field['city'] = help_noempty(field['city']);
 	check_field['zip_code'] = help_noempty(field['zip_code']);
@@ -199,21 +260,33 @@ function check_interests(interests){
 }
 
 async function get_id_user (login){
-	var sql = "SELECT `id` FROM `user` WHERE `login` LIKE ?";
-	var todo = [login];
-	result = await db.query(sql, todo);
-	return (result[0].id);
+	try {
+		var sql = "SELECT `id` FROM `user` WHERE `login` LIKE ?";
+		var todo = [login];
+		result = await db.query(sql, todo);
+		return (result[0].id);
+	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
 }
 
-exports.get_photo = async function (login){
-	id_login = await get_id_user(login);
-	var sql = "SELECT * FROM `photos` WHERE `id_user` = ?";
-	var todo = [id_login];
-	result = await db.query(sql, todo);
-	return (result);
+async function get_photo (login){
+	try {
+		id_login = await get_id_user(login);
+		var sql = "SELECT * FROM `photos` WHERE `id_user` = ?";
+		var todo = [id_login];
+		result = await db.query(sql, todo);
+		return (result);
+	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
 }
 
-exports.check_picture = function (files, login, number){
+function check_picture (files, login, number){
 	var check_pic = "ok";
 	if ((files == undefined || files.photo == undefined || files.photo.size == 0) && number == 0){
 		check_pic = "no_pic_uploaded";
@@ -230,169 +303,230 @@ exports.check_picture = function (files, login, number){
 	return check_pic;
 }
 
-exports.isCompleted = async function (login){
+async function isCompleted (login){
+	try {
 
-	var userExist = await is_info_user_exist(login);
-	var id_user = await get_id_user(login);
+		var userExist = await is_info_user_exist(login);
+		var id_user = await get_id_user(login);
 
 
-	if (userExist == true) {
-		var sql = "UPDATE `user_info` SET `completed` = 1 WHERE `id_user` = ?";
-		var todo = [id_user];
-		result = await db.query(sql, todo);
+		if (userExist == true) {
+			var sql = "UPDATE `user_info` SET `completed` = 1 WHERE `id_user` = ?";
+			var todo = [id_user];
+			result = await db.query(sql, todo);
+		}
+		else{
+			var sql = "INSERT INTO `user_info` ( `completed`,  `id_user`) VALUES (?, ?)";
+			var todo = [id_user];
+			result = await db.query(sql, todo);
+		}
+
 	}
-	else{
-		var sql = "INSERT INTO `user_info` ( `completed`,  `id_user`) VALUES (?, ?)";
-		var todo = [id_user];
-		result = await db.query(sql, todo);
+	catch (err){
+		console.log(err);
+		return (err);
 	}
-
 }
 
 async function update_info_user (login, property, field){
-	if (property != "interests"){
-		id_user = await get_id_user(login);
-		var sql = "UPDATE `user_info` SET `"+property+"` = ? WHERE `id_user` = ?";
-		var todo = [field, id_user];
-		result = await db.query(sql, todo);
-	}
-	else{
-		interests = field;
-		var sql = "UPDATE `user_info` SET `interests` = ?  WHERE `id_user` = ?";
-		inter = "";
-		itemsProcessed = 0;
-		if (interests != undefined && interests != null && Array.isArray(interests)){
-			interests.forEach(async function(interest) {
-				if (inter == ""){
-					inter += interest;
-				}else{
-					inter += "," + interest;
-				}
-				itemsProcessed++;
-				if(itemsProcessed === interests.length) {
-					var todo = [inter,id_user];
-					result = await db.query(sql, todo);
-				}
-			});
-		}
-		else {
-			inter = interests;
-			var todo = [inter, id_user];
+	try {
+		if (property != "interests"){
+			id_user = await get_id_user(login);
+			var sql = "UPDATE `user_info` SET `"+property+"` = ? WHERE `id_user` = ?";
+			var todo = [field, id_user];
 			result = await db.query(sql, todo);
 		}
+		else{
+			interests = field;
+			var sql = "UPDATE `user_info` SET `interests` = ?  WHERE `id_user` = ?";
+			inter = "";
+			itemsProcessed = 0;
+			if (interests != undefined && interests != null && Array.isArray(interests)){
+				interests.forEach(async function(interest) {
+					if (inter == ""){
+						inter += interest;
+					}else{
+						inter += "," + interest;
+					}
+					itemsProcessed++;
+					if(itemsProcessed === interests.length) {
+						var todo = [inter,id_user];
+						result = await db.query(sql, todo);
+					}
+				});
+			}
+			else {
+				inter = interests;
+				var todo = [inter, id_user];
+				result = await db.query(sql, todo);
+			}
+		}
+	}
+	catch (err){
+		console.log(err);
+		return (err);
 	}
 }
 
 async function insert_info_user (login, property, field){
-	if (property != "interests"){
-		id_user = await get_id_user(login);
-		var sql = "INSERT INTO `user_info` (`id_user`, `"+property+"`) VALUES (?, ?);";
-		var todo = [id_user, field];
-		result = await db.query(sql, todo);
-	}
-	else
-	{
-		interests = field;
-		var sql = "INSERT INTO `user_info` (`id_user`, `interests`) VALUES (?, ?);";
-		inter = "";
-		itemsProcessed = 0;
-		if (interests !=undefined && interests != null && Array.isArray(interests)){
-			interests.forEach(async function(interest) {
-				if (inter == ""){
-					inter += interest;
-				}else{
-					inter += "," + interest;
-				}
-				itemsProcessed++;
-				if(itemsProcessed === interests.length) {
-					var todo = [id_user, inter];
-					result = await db.query(sql, todo);
-				}
-			});
-		}
-		else {
-			inter = interests;
-			var todo = [id_user, inter];
+	try {
+		if (property != "interests"){
+			id_user = await get_id_user(login);
+			var sql = "INSERT INTO `user_info` (`id_user`, `"+property+"`) VALUES (?, ?);";
+			var todo = [id_user, field];
 			result = await db.query(sql, todo);
 		}
+		else
+		{
+			interests = field;
+			var sql = "INSERT INTO `user_info` (`id_user`, `interests`) VALUES (?, ?);";
+			inter = "";
+			itemsProcessed = 0;
+			if (interests !=undefined && interests != null && Array.isArray(interests)){
+				interests.forEach(async function(interest) {
+					if (inter == ""){
+						inter += interest;
+					}else{
+						inter += "," + interest;
+					}
+					itemsProcessed++;
+					if(itemsProcessed === interests.length) {
+						var todo = [id_user, inter];
+						result = await db.query(sql, todo);
+					}
+				});
+			}
+			else {
+				inter = interests;
+				var todo = [id_user, inter];
+				result = await db.query(sql, todo);
+			}
+		}
+	}
+	catch (err){
+		console.log(err);
+		return (err);
 	}
 }
 
 async function count_photo (id_user){
-	var sql = "SELECT count(*) as 'count' FROM `photo` WHERE `id_user` = ?";
-	var todo = [id_user];
-	result = await db.query(sql, todo);
-	if (result[0] == undefined) {
-		return (0);
+	try {
+		var sql = "SELECT count(*) as 'count' FROM `photo` WHERE `id_user` = ?";
+		var todo = [id_user];
+		result = await db.query(sql, todo);
+		if (result[0] == undefined) {
+			return (0);
+		}
+		else {
+			return (result[0].count);
+		}
 	}
-	else {
-		return (result[0].count);
+	catch (err){
+		console.log(err);
+		return (err);
 	}
 }
 
 async  function is_info_user_exist (login){
-	id_user = await get_id_user(login);
-	var sql = "SELECT * FROM `user_info` WHERE `id_user` = ?";
-	var todo = [id_user];
-	result = await db.query(sql, todo);
-	if (result[0] == undefined) {
-		return (false);
-	}
-	else {
-		return (true);
-	}
-}
-
-exports.savethePic = async function (files, login, number){
-	var profile = 0;
-	if (number == 0){
-		profile = 1;
-	}
-	name = rootPath+"/public/photo/"+login+"/"+number;
-	files.photo.mv(name);
-	done = await savePic(id_user, "/public/photo/"+login+"/"+number, profile)
-	return done;
-}
-
-exports.get_info_user = async function (login){
-	var user = await get_info_user(login);
-	user = user[0];
-	for (const property in user){
-		if(user[property] != null){
-			user[property] = user[property];
+	try {
+		id_user = await get_id_user(login);
+		var sql = "SELECT * FROM `user_info` WHERE `id_user` = ?";
+		var todo = [id_user];
+		result = await db.query(sql, todo);
+		if (result[0] == undefined) {
+			return (false);
 		}
-		else{
-			user[property] = false;
+		else {
+			return (true);
 		}
 	}
-	if (user != undefined && user.birth != undefined && user.birth != false)
-	{
-		user.birthday = user.birth;
-		user.birthday = user.birthday.replace("\\", "/");
-		user.birthday = user.birthday.replace("\\", "/");
-		user.birthday = user.birthday.replace("\\", "/");
-		user.birthday = user.birthday.split("/");
-		user.birthday = user.birthday[1]+ "/"+ user.birthday[2]+"/"+user.birthday[0];
+	catch (err){
+		console.log(err);
+		return (err);
 	}
-	if (user != undefined && user.interests != false){
-		user.interests = user.interests.split(",");
-	}
-	id_user = await get_id_user(login);
-	pic = await getPic(id_user);
-	if (user == undefined)
-	{
-		user = {};
-	}
-	if (pic != false){
-		user.pic = pic;
-	}
-	return user;
 }
 
-exports.get_completed = async function (login){
-	id_user = await get_id_user(login);
-	var sql = "SELECT `completed` FROM `user_info` WHERE `id_user` = ?";
-	var todo = [id_user];
-	result = await db.query(sql, todo);
-	return (result[0]);
+async function savethePic (files, login, number){
+	try {
+		var profile = 0;
+		if (number == 0){
+			profile = 1;
+		}
+		name = rootPath+"/public/photo/"+login+"/"+number;
+		files.photo.mv(name);
+		done = await savePic(id_user, "/public/photo/"+login+"/"+number, profile)
+		return done;
+	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
+}
+async function get_the_info_user (login){
+	try {
+		var id_user = await get_id_user(login);
+		var sql = "SELECT *, DATE_FORMAT(`birthday`, '%Y\\%m\\%d') as birth FROM `user_info` WHERE `id_user` = ?";
+		var todo = [id_user];
+		var result = await db.query(sql, todo);
+		return (result);
+	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
+}
+
+async function get_info_user (login){
+	try {
+		var user = await get_the_info_user(login);
+		user = user[0];
+		for (const property in user){
+			if(user[property] != null){
+				user[property] = user[property];
+			}
+			else{
+				user[property] = false;
+			}
+		}
+		if (user != undefined && user.birth != undefined && user.birth != false)
+		{
+			user.birthday = user.birth;
+			user.birthday = user.birthday.replace("\\", "/");
+			user.birthday = user.birthday.replace("\\", "/");
+			user.birthday = user.birthday.replace("\\", "/");
+			user.birthday = user.birthday.split("/");
+			user.birthday = user.birthday[1]+ "/"+ user.birthday[2]+"/"+user.birthday[0];
+		}
+		if (user != undefined && user.interests != false){
+			user.interests = user.interests.split(",");
+		}
+		id_user = await get_id_user(login);
+		pic = await getPic(id_user);
+		if (user == undefined)
+		{
+			user = {};
+		}
+		if (pic != false){
+			user.pic = pic;
+		}
+		return user;
+	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
+}
+
+async function get_completed (login){
+	try {
+		id_user = await get_id_user(login);
+		var sql = "SELECT `completed` FROM `user_info` WHERE `id_user` = ?";
+		var todo = [id_user];
+		result = await db.query(sql, todo);
+		return (result[0]);
+	}
+	catch (err){
+		console.log(err);
+		return (err);
+	}
 }
